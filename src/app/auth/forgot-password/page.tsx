92a0aae27fa71a4686b31d3bda7ff9edad9648c6
Vidit Aggarwal <shrividhyaclasses@gmail.com>
2025-09-09 15:16:36 +0530

    Fix forgot password page

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -46,7 +46,7 @@ export default function ForgotPasswordPage() {
           <Mail className="w-12 h-12 text-[#e27447] mx-auto mb-4" />
           <CardTitle className="text-xl text-[#1e293b]">Forgot Password?</CardTitle>
           <CardDescription>
-            Enter your email address and we'll send you a link to reset your password.
+            Enter your email address and we&apos;ll send you a link to reset your password.
           </CardDescription>
         </CardHeader>
         <CardContent>
